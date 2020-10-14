6cc7010eae90102a0ce037d4861264b0590070a7
LukasSonnabend <lukassonnabend@outlook.com>
2020-10-14 22:51:14 +0200

    Updated label to new 10mb upload size

--- tokenstartfrontend/src/components/pages/createProject.jsx
+++ tokenstartfrontend/src/components/pages/createProject.jsx
@@ -117,7 +117,8 @@ export default function CreateProject() {
                                 withIcon={true}
                                 singleImage={true}
                                 onChange={onDrop}
-                                imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+                                label="Max file size: 10mb, accepted: jpg|gif|png"
+                                imgExtension={[".jpg", ".gif", ".png"]}
                                 maxFileSize={10485760}
                             />
                         }
