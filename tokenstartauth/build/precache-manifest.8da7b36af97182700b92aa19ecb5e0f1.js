388e7a47e175d6fdd2873afceffecaa69edc5991
LukasSonnabend <lukassonnabend@outlook.com>
2020-10-20 09:48:59 +0200

    updates

---
+++ tokenstartauth/build/precache-manifest.8da7b36af97182700b92aa19ecb5e0f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86d79053627c57172d5606214f5d6cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45238181229938e20086",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "27b7a6c0b05d68b31ba1",
+    "url": "/static/css/main.7993c17b.chunk.css"
+  },
+  {
+    "revision": "45238181229938e20086",
+    "url": "/static/js/2.783d76bf.chunk.js"
+  },
+  {
+    "revision": "9f90cde4396c52b73cc0a6bcd8267b7b",
+    "url": "/static/js/2.783d76bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b7a6c0b05d68b31ba1",
+    "url": "/static/js/main.79c56331.chunk.js"
+  },
+  {
+    "revision": "d91716230a4e9dd3ca32",
+    "url": "/static/js/runtime-main.e0b531ef.js"
+  },
+  {
+    "revision": "2b201eff748d6e7ffc6b298fe27dc090",
+    "url": "/static/media/Founder_Konstantin.2b201eff.png"
+  },
+  {
+    "revision": "9191689860af639fcb87cd76b5cd9447",
+    "url": "/static/media/Founder_Lukas.91916898.jpg"
+  },
+  {
+    "revision": "48c161df9991f9b0f6e4a858e95e415e",
+    "url": "/static/media/UbuntuItalic.48c161df.ttf"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
